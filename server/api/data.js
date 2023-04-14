c2dcf8aab1e3b361eea36aa95ea1d134f5960703
Yingson Yu <yingsonyu@gmail.com>
2023-04-14 16:38:41 -0500

    secret

--- server/api/data.js
+++ server/api/data.js
@@ -3,14 +3,19 @@ const { spawn } = require("child_process");
 
 module.exports = router;
 
-router.all("/", (req, res) => {
+router.all("/sendstock/:secretkey", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  const secret = req.params.secretkey;
+
+  if (secret !== process.env.SECRET_KEY) {
+    res.send("acccess denied").status(403);
+    return;
+  }
+
   const data = req.body.data;
   const location = req.body.location;
-
-  console.log(data);
-  console.log(location);
 
   let largeDataSet = [];
   let result = "";
@@ -29,44 +34,58 @@ router.all("/", (req, res) => {
     result = data;
   });
 
-  console.log(largeDataSet);
-
   // in close event we are sure that stream is from child process is closed
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    // res.send(largeDataSet.join(""));
+
     res.send(result);
   });
 });
 
-router.all("/fetchlocations", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  let result = "";
-  // spawn new child process to call the python script
-  const python = spawn("python3", ["server/api/get_list_of_locations.py"]);
+router.all("/fetchlocations/:secretkey", (req, res, next) => {
+  const secret = req.params.secretkey;
 
-  // collect data from script
-  python.stdout.on("data", function (data) {
-    console.log("Pipe data from python script ...");
-    //dataToSend =  data;
-    // largeDataSet.push(data);
-    result = data;
-  });
+  if (secret !== process.env.SECRET_KEY) {
+    res.send("access denied").status(403);
+    return;
+  }
 
-  // in close event we are sure that stream is from child process is closed
-  python.on("close", (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    // res.send(largeDataSet.join(""));
-    res.send(result);
-  });
+  try {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    let result = "";
+    // spawn new child process to call the python script
+    const python = spawn("python3", ["server/api/get_list_of_locations.py"]);
+
+    // collect data from script
+    python.stdout.on("data", function (data) {
+      result = data;
+    });
+
+    // in close event we are sure that stream is from child process is closed
+    python.on("close", (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+      // send data to browser
+      // res.send(largeDataSet.join(""));
+      res.send(result);
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.all("/fetchstock/:location", (req, res) => {
+router.all("/fetchstock/:location/:secretkey", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  const secret = req.params.secretkey;
+
+  if (secret !== process.env.SECRET_KEY) {
+    res.send("acccess denied").status(403);
+    return;
+  }
+
   let largeDataSet = [];
   let result = "";
   // spawn new child process to call the python script
@@ -78,12 +97,9 @@ router.all("/fetchstock/:location", (req, res) => {
   // collect data from script
   python.stdout.on("data", function (data) {
     console.log("Pipe data from python script ...");
-    //dataToSend =  data;
-    // largeDataSet.push(data);
+
     result = data;
   });
-
-  console.log(largeDataSet);
 
   // in close event we are sure that stream is from child process is closed
   python.on("close", (code) => {
--- server/index.js
+++ server/index.js
@@ -3,15 +3,12 @@ const app = express();
 const morgan = require("morgan");
 const parser = require("body-parser");
 const port = process.env.PORT || 4001;
+require("dotenv").config();
 
 app.use(morgan("dev"));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
-// /assets virtual path for the images
 app.use("/api", require("./api/pyroute"));
 
 app.listen(port, () => console.log("listening on port " + port));
-
-// use vite's connect instance as middleware
-// if you use your own express router (express.Router()), you should use router.use
